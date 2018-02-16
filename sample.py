b3c3dd8a6f01578befd7a02d2a5ea601818c2db4
steve <stevewang@outlook.com>
2018-02-16 18:40:20 +0800

    version 0.1

--- sample.py
+++ sample.py
@@ -1,122 +1,111 @@
 #! /usr/local/bin/python3
 
 # example file for powerpoint slides generation
-import random
-import os
 from pptx import Presentation
 from pptx.util import Inches
 
-# if font operation required, pls include below import statement
-from openpyxl.styles import Font
+# more modules as reference below
+from pptx.util import Pt
+from pptx.dml.color import RGBColor
+from pptx.enum.dml import MSO_THEME_COLOR
 
-class WorkBook():
-	''' WorkBook is to wrap the workbook. each instance is a workbook
+# chart data related
+# from pptx.chart.data import ChartData
+# from pptx.enum.chart import XL_TICK_MARK
+# from pptx.enum.chart import XL_DATA_LABEL_POSITION
+# from pptx.enum.chart import XL_LEGEND_POSITON
+# from pptx.enum.chart import XL_CHART_TYPE
+# from pptx.enum.chart import XL_MARKER_STYLE
+
+# define variables
+COVER_PAGE_LAYOUT = 0
+DELIVERY_PAGE_LAYOUT = 1
+CONTENT_PAGE_LAYOUT = 2
+COVER_TITLE_IDX = 13
+COVER_SUBTITLE_IDX = 14
+
+class AutoSlide():
+	''' class to wrap slides related function
 	'''
-
-	def __init__(self,filename=None):
-		''' if filename not provided, a new workbook would be created
-		'''
-		self.status = False
-
-		if not filename:
-			self.filename = "temp.xls.file.%04d.xlsx" % random.randint(1,1000) 
-			self.workbook = openpyxl.Workbook()
-			self.workbook.create_sheet(title="Sheet1", index=0)
-			self.workbook.save(self.filename)
+	def __init__(self, filename = None):
+		if filename:
+			self.templatefn = filename
 		else:
-			self.filename = filename
-		self.load_excel()
+			self.templatefn = "template.pptx"
+		self.presentation = Presentation(self.templatefn)
+		print("Initiate presentation w/ %s" % self.templatefn)
+		self.layouts = self.presentation.slide_layouts
+		self.slides = self.presentation.slides
 
-	def load_excel(self):
-		''' load excel sheets into the instance
-		'''
-		try:
-			self.workbook = openpyxl.load_workbook(self.filename)
-			self.status = True
-			print('[%s] loaded. %d sheets inside: %s' % (self.filename , len(self.workbook.sheetnames),self.workbook.sheetnames))
-		except FileNotFoundError:
-			print('Load error: %s not found' % self.filename)
+	def add_cover_page(self, title, subtitle):
+		cover_page_layout = self.layouts[COVER_PAGE_LAYOUT]
+		self.coverpage = self.slides.add_slide(cover_page_layout)
+		for shape in self.coverpage.placeholders:
+			print('<DEBUG placeholder> %d:%s' % (shape.placeholder_format.idx, shape.name))
+		# third, set title and subtitle
+		self.coverpage.placeholders[COVER_TITLE_IDX].text = title 
+		self.coverpage.placeholders[COVER_SUBTITLE_IDX].text = subtitle
+		self.coverpage.name = "Cover"
 
-	def get_sheet(self, index=0):
-		if not self.status:
-			return
-		self.sheet = self.workbook[self.workbook.sheetnames[index]]
-		print('Sheet loaded, data range: [%d:%d]' % (self.sheet.max_row,self.sheet.max_column))
-		print('Sammple value in [A1 / B3] : [%s / %s]' % ( self.sheet['A1'].value, self.sheet['B3'].value))
+	def add_delivery_page(self, page_title, column_titles, column_widths):
+		# add the page
+		self.delivery_page = self.slides.add_slide(self.layouts[DELIVERY_PAGE_LAYOUT])
+		self.delivery_page.name = "Delivery"
 
-	def manipulate_and_save(self):
-		''' sample code of data manipulation
-		'''
-		print("SET TITLE / DATA; CREATE NEW SHEET; SET DIMENSION AND FONT")
-		self.sheet.title = 'SAMPLE TITLE'
-		self.sheet['A1'] = 1999
-		self.sheet['B3'] = 'aloha'
-		for i in range(8,21):
-			for j in range(1,11):
-				self.sheet.cell(row = i, column = j).value = i*10+j
-		self.workbook.create_sheet(title='new sheet' ,index = 1)
-		self.sheet.column_dimensions['B'].width = 40
-		self.sheet.row_dimensions[1].height = 30
-		self.sheet['B3'].font = Font(sz=18,bold=True, italic=True)
-		self.workbook.save(self.filename + '.out.xlsx')
-
-def fill_deliverable_page(shapes):
-	shapes.title.text= "Deliverable Tracking"
-	cats = ("Deliverables",
-		"Issue / Concern",
-		"Mitigation Plan",
-		"Action / Key Ask",
-		"Owner",
-		"Due Date",
-		"S",
-		"Status")
-	widths = (1.5,2.5,1.5,2,1.5,1.5,0.3,1)
-
-	row = 10 
-	col = len(cats)
+		# set page title
+		shapes = self.delivery_page.shapes
+		shapes.title.text= page_title 
+		
+		# create table
+		num_row = 10 
+		num_col = len(column_titles)
 	
-	left = Inches(1)
-	top = Inches(1.3)
-	width = Inches(7.0)
-	height = Inches(0.8)
+		left = Inches(1)
+		top = Inches(1.3)
+		width = Inches(7.0)
+		height = Inches(0.8)
 	
-	table = shapes.add_table(row,col,left,top,width,height).table
+		table = shapes.add_table(num_row,num_col,left,top,width,height).table
 
-	i = 0
-	for cat in cats:
-		table.cell(0,i).text = cat
-		table.columns[i].width = Inches(widths[i])
-		i += 1
-
+		# set value for table titles
+		i = 0
+		for title in column_titles:
+			table.cell(0,i).text = title
+			table.cell(0,i).fill.solid()
+			table.cell(0,i).fill.fore_color.rgb = RGBColor(0x0,0x0,0x5f)
+			table.cell(0,i).text_frame.paragraphs[0].font.size = Pt(12)
+			table.columns[i].width = Inches(column_widths[i])
+			i += 1
+		table.cell(1,len(column_titles)-2).fill.solid()
+		table.cell(1,len(column_titles)-2).fill.fore_color.rgb = RGBColor(0xff,0x0,0x0)
+		
 # unit test code below
 if __name__ == '__main__':
 
-	#curdir = os.path.dirname(os.path.realpath('.'))
-	#print("Current Dir: %s" % curdir)
+	# new an presentation object
+	presentation = AutoSlide('template.pptx')
 
-	# first, new an presentation object
-	prs = Presentation('template.pptx')
-
-	# second, get layout and use it to generate one page
-	cover_page_layout = prs.slide_layouts[0]
-	cover_page= prs.slides.add_slide(cover_page_layout)
-
-	deliverable_page_layout = prs.slide_layouts[2]
-	deliverable_page= prs.slides.add_slide(deliverable_page_layout)
-
-	print("%d placeholders inside" % len(cover_page.placeholders))
+	# prapare data for cover page
+	title = "5GTF STREAM-1 SUBSTREAM-2 CORE UPSTREAM WEEKLY" 
+	subtitle = "by xBG solution team led by MN PS"
 	
-	for shape in cover_page.placeholders:
-		print('%d %s' % (shape.placeholder_format.idx, shape.name))
-	
-	print("%d shapes inside" % len(deliverable_page.shapes))
-	
-	for shape in deliverable_page.placeholders:
-		print('%d %s' % (shape.placeholder_format.idx, shape.name))
+	# Generate cover page
+	presentation.add_cover_page(title,subtitle)
 
-	table = fill_deliverable_page(deliverable_page.shapes)
-	# third, set title and subtitle
-	cover_page.placeholders[13].text = "5GTF STREAM-1 SUBSTREAM-2 CORE UPSTREAM WEEKLY" 
-	cover_page.placeholders[14].text = "by xBG solution team led by MN PS"
+	# prepare dat for deliverable page
+	page_title = "Deliverable Tracking"
+	column_titles = ("Deliverables",
+			"Issue / Concern",
+			"Mitigation Plan",
+			"Action / Key Ask",
+			"Owner",
+			"Due Date",
+			"S",
+			"Status")
+	column_widths = (1.5,2.5,1.5,2,1.5,1.5,0.3,1)
 
-	prs.save('test.pptx')
+	# generate deliverable taracking page
+	presentation.add_delivery_page(page_title,column_titles, column_widths)
+
+	# save the generated file
+	presentation.presentation.save('test.pptx')
